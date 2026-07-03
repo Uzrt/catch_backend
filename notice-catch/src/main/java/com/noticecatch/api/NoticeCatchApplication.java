3f98cc8706ecd53df80155594cbbbe42ec02c5ae
Uzrt <worldsilvertree@gmail.com>
2026-07-04 00:18:40 +0900

    프로젝트 기본 세팅

---
+++ notice-catch/src/main/java/com/noticecatch/api/NoticeCatchApplication.java
@@ -0,0 +1,13 @@
+package com.noticecatch.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoticeCatchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NoticeCatchApplication.class, args);
+    }
+
+}
---
+++ notice-catch/src/test/java/com/noticecatch/api/NoticeCatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.noticecatch.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoticeCatchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
